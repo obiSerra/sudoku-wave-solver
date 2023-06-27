21f331c8f07724980bf7812c3a73e5ceaff4f8a2
Roberto Serra <roberto.serra@etiqa.it>
2023-06-27 16:34:32 +0200

    split code in submodules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-function sample(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
+const { sample, SudokuBoard, listPossibleValues } = require("./SudokuBoard.js");
 
 const emptyBoard = [
   [null, null, null, null, null, null, null, null, null],
@@ -13,114 +11,6 @@ const emptyBoard = [
   [null, null, null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null, null, null],
 ];
-
-class SudokuBoard {
-  constructor(board = emptyBoard) {
-    this.board = board;
-  }
-
-  cols() {
-    return this.board.map((_, i) => this.board.map(row => row[i]));
-  }
-
-  rows() {
-    return this.board;
-  }
-
-  squares() {
-    const squares = [];
-    const board = this.board;
-    for (let i = 0; i < 9; i++) {
-      const square = [];
-      for (let j = 0; j < 9; j++) {
-        const row = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-        const col = (i % 3) * 3 + (j % 3);
-        square.push(board[row][col]);
-      }
-      squares.push(square);
-    }
-
-    return squares;
-  }
-
-  cellRow(cell) {
-    return this.board[cell[0]];
-  }
-
-  cellCol(cell) {
-    return this.cols()[cell[1]];
-  }
-
-  cellSquare(cell) {
-    const indx = Math.floor(cell[0] / 3) * 3 + Math.floor(cell[1] / 3);
-    return this.squares()[indx];
-  }
-
-  print() {
-    console.log("=========================================");
-    this.rows().forEach((row, i) => {
-      const r = row.map((cell, j) => {
-        const out = cell ? cell : " ";
-        return out + (j % 3 === 2 ? " ||" : " |");
-      });
-      console.log("|| " + r.join(" "));
-      if (i == 8) {
-      } else if (i % 3 === 2) {
-        console.log("||===|===|===||===|===|===||===|===|===||");
-      } else {
-        console.log("||---|---|---||---|---|---||---|---|---||");
-      }
-    });
-    console.log("=========================================");
-  }
-
-  _isValid(arr) {
-    return arr
-      .map(a => {
-        const vals = a.filter(b => b !== null);
-        return (
-          Array.from(new Set(vals)).length == vals.length &&
-          vals
-            .map(b => {
-              return b < 10;
-            })
-            .every(b => b)
-        );
-      })
-      .every(b => b);
-  }
-
-  isBoardValid() {
-    return this._isValid(this.rows()) && this._isValid(this.cols()) && this._isValid(this.squares());
-  }
-
-  missingNumbers() {
-    const missing = [];
-    for (let i = 0; i < this.board.length; i++) {
-      const row = this.board[i];
-      for (let j = 0; j < row.length; j++) {
-        const val = row[j];
-        if (!val) {
-          missing.push([i, j]);
-        }
-      }
-    }
-    return missing;
-  }
-}
-
-const listPossibleValues = (board, cell) => {
-  const row = board.cellRow(cell);
-  const col = board.cellCol(cell);
-  const square = board.cellSquare(cell);
-  const all = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-  let missing = all
-    .filter(a => !row.includes(a))
-    .filter(a => !col.includes(a))
-    .filter(a => !square.includes(a));
-  return missing;
-};
 
 const solveWave = (board, invalid = {}, calls = []) => {
   const missing = board.missingNumbers();
@@ -216,6 +106,14 @@ module.exports = {
       throw new Error("The board is not valid");
     }
     const solvedBoard = solveWaveIterative(board);
+    return solvedBoard.board;
+  },
+  solveRecursive: boardJson => {
+    const board = new SudokuBoard(boardJson);
+    if (!board.isBoardValid()) {
+      throw new Error("The board is not valid");
+    }
+    const solvedBoard = solveWave(board);
     return solvedBoard.board;
   },
   SudokuBoard,
--- sudoku_sover.js
+++ sudoku_sover.js
@@ -4,37 +4,26 @@ const fs = require("fs/promises");
 (async () => {
   const games = [
     [
-      [null, 9, 4, null, 6, 2, null, null, null],
-      [2, null, 3, null, null, null, 8, null, 6],
-      [5, null, 6, 7, null, 1, null, 9, null],
-      [6, null, null, 1, null, null, null, 2, 7],
-      [null, null, 1, 4, null, 3, null, null, null],
-      [9, null, null, null, 5, null, 3, 8, null],
-      [null, 7, 2, null, null, 8, null, 1, null],
-      [3, 5, null, null, null, 7, 9, null, 2],
-      [null, null, null, 3, null, null, null, 5, null],
+      [8, null, null, null, null, null, null, null, null],
+      [null, null, 3, 6, null, null, null, null, null],
+      [null, 7, null, null, 9, null, 2, null, null],
+      [null, 5, null, null, null, 7, null, null, null],
+      [null, null, null, null, 4, 5, 7, null, null],
+      [null, null, null, 1, null, null, null, 3, null],
+      [null, null, 1, null, null, null, null, 6, 8],
+      [null, null, 8, 5, null, null, null, 1, null],
+      [null, 9, null, null, null, null, 4, null, null],
     ],
-
-    // [
-    //   [8, null, null, null, null, null, null, null, null],
-    //   [null, null, 3, 6, null, null, null, null, null],
-    //   [null, 7, null, null, 9, null, 2, null, null],
-    //   [null, 5, null, null, null, 7, null, null, null],
-    //   [null, null, null, null, 4, 5, 7, null, null],
-    //   [null, null, null, 1, null, null, null, 3, null],
-    //   [null, null, 1, null, null, null, null, 6, 8],
-    //   [null, null, 8, 5, null, null, null, 1, null],
-    //   [null, 9, null, null, null, null, 4, null, null],
-    // ],
   ];
 
   for (const game of games) {
     const board = new SudokuBoard(game);
 
     board.print();
-    const solved = solve(game);
+    const time = Date.now();
+    const solved = solveRecursive(game);
+    console.log("Solved in " + (Date.now() - time) + "ms");
     const solvedBoard = new SudokuBoard(solved);
     solvedBoard.print();
-    console.log("\n\n=========================================\n\n");
   }
 })();
